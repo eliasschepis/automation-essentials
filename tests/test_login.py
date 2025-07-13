15ad94580e3db7dabcaccd72270ff5f3b7dc1bae
Elias Schepis <schepiselias@gmail.com>
2025-07-13 17:28:28 +0200

    Update test_login.py

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,3 +1,15 @@
+# Importamos los localizadores por tipo (ID, XPATH, etc.)
+from selenium.webdriver.common.by import By
+
+# Importamos el driver base para tipar correctamente
+from selenium.webdriver.remote.webdriver import WebDriver
+
+# Esperas explícitas para asegurar que los elementos estén presentes/interactuables
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Test
+
 def test_login_valid_user(driver):  # El driver es una fixture
     login_page = LoginPage(driver)  # Instanciamos la clase LoginPage
     login_page.login("standard_user", "secret_sauce")  # Ejecutamos el flujo
